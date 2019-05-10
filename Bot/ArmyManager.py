ea22a0d3e1f67e8a75ed9d0ca33b49fe3ba47657
kohlerphilipp <philipp.kohler@uni-ulm.de>
2019-05-10 12:10:20 +0200

    Now with Immortal and better Pylon Count

--- Bot/ArmyManager.py
+++ Bot/ArmyManager.py
@@ -1,19 +1,29 @@
 import sc2
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, WARPGATE, STALKER, WARPGATETRAIN_STALKER
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, WARPGATE, STALKER, \
+    WARPGATETRAIN_STALKER, ROBOTICSFACILITY, IMMORTAL
 import random
+
 
 # Build the Army
 async def build_offensive_force(self):
-    for gw in self.units(GATEWAY).ready.noqueue:
+    if self.time > 360 and self.units(NEXUS).amount < 3:
+        return
 
+    for robo in self.units(ROBOTICSFACILITY).ready.noqueue:
+        if self.can_afford(IMMORTAL) and self.supply_left > 0:
+            await self.do(robo.train(IMMORTAL))
+
+    for gw in self.units(GATEWAY).ready.noqueue:
         if self.can_afford(STALKER) and self.supply_left > 0:
-            await self.do(gw.train(STALKER))
+            if not self.warpgateresearched:
+                await self.do(gw.train(STALKER))
 
     for wg in self.units(WARPGATE).ready.noqueue:
         pylon = self.units(PYLON).ready.random.position
         placement = await self.find_placement(WARPGATETRAIN_STALKER, near=pylon, placement_step=1)
         if self.can_afford(STALKER) and self.supply_left > 0:
-            await self.do(wg.warp_in(STALKER, placement))
+            if self.units(IMMORTAL).amount >= 2 or self.units(ROBOTICSFACILITY).ready.noqueue.amount == 0:
+                await self.do(wg.warp_in(STALKER, placement))
 
 
 def find_target(self, state):
@@ -25,14 +35,16 @@ def find_target(self, state):
         return self.enemy_start_locations[0]
 
 
+# ToDo find way to group army
 async def attack(self):
     # {UNIT: [n to fight, m to defend]}
-    aggressive_units = {STALKER: [15, 1]}
+    aggressive_units = {STALKER: [15, 1],
+                        IMMORTAL: [3, 0]}
 
     for UNIT in aggressive_units:
         if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
             for s in self.units(UNIT).idle:
-                await self.do(s.attack(self.find_target(self.state)))
+                await self.do(s.attack(find_target(self, self.state)))
 
         elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
             if len(self.known_enemy_units) > 0:
--- Bot/BaseManager.py
+++ Bot/BaseManager.py
@@ -1,10 +1,10 @@
 import sc2
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, WARPGATE
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, WARPGATE, ROBOTICSFACILITY
 
 
-# Build 22 Probes per Nexus
+# Build 44 Probes
 async def build_workers(self):
-    if (len(self.units(NEXUS)) * 22) > len(self.units(PROBE)):
+    if 44 > len(self.units(PROBE)):
         for nexus in self.units(NEXUS).ready.noqueue:
             if self.can_afford(PROBE):
                 await self.do(nexus.train(PROBE))
@@ -12,10 +12,9 @@ async def build_workers(self):
 
 # Build Pylon if under 5 Supply left
 async def build_pylons(self):
-    if self.supply_left < 5 and len(self.units(PYLON).not_ready) < 2:
+    if self.supply_left < 5 and self.can_afford(PYLON) and not self.already_pending(PYLON):
         nexus = self.units(NEXUS).random
-        if self.can_afford(PYLON):
-            await self.build(PYLON, near=nexus.position.towards(self.game_info.map_center, 8))
+        await self.build(PYLON, near=nexus.position.towards(self.game_info.map_center, 8))
 
 
 # ToDo search for better way to take Vaspenes
@@ -27,7 +26,7 @@ async def build_assimilators(self):
                 worker = self.select_build_worker(vaspene.position)
                 await self.do(worker.build(ASSIMILATOR, vaspene))
 
-            if self.can_afford(ASSIMILATOR) and self.units(NEXUS).amount > 1 and self.units(ASSIMILATOR).amount < 4:
+            if self.can_afford(ASSIMILATOR) and self.units(NEXUS).amount > 1 and self.units(ASSIMILATOR).amount < 3:
                 worker = self.select_build_worker(vaspene.position)
                 await self.do(worker.build(ASSIMILATOR, vaspene))
 
@@ -59,6 +58,10 @@ async def offensive_force_buildings(self):
             if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                 await self.build(GATEWAY, near=pylon)
 
-        if len(self.units(GATEWAY)) + len(self.units(WARPGATE)) < 8 and self.units(NEXUS).amount == 2:
+        if len(self.units(GATEWAY)) + len(self.units(WARPGATE)) < 6 and self.units(NEXUS).amount >= 2:
             if self.warpgateresearched and self.can_afford(GATEWAY):
                 await self.build(GATEWAY, near=pylon)
+
+        if self.units(ROBOTICSFACILITY).amount < 2 and self.units(NEXUS).amount >= 2:
+            if self.warpgateresearched and self.can_afford(ROBOTICSFACILITY):
+                await self.build(ROBOTICSFACILITY, near=pylon)
